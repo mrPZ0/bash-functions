2706a95fd32e35a9ea93bae886036f3acdaf8d0a
mrPZ <mrpz@ya.ru>
2023-06-08 08:07:04 +0300

    upd: rename functions

--- base-functions.sh
+++ base-functions.sh
@@ -2,26 +2,28 @@
 #
 # default functions for scripts
 #
-
-
-# function err()
-# 
-#
-# if ! do_something; then
-#   err "Unable to do_something"
-#   exit 1
-# fi
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
-}
-
+################################################################################
+base_functions_ver=2
 red="\e[31m"
 green="\e[32m"
 endcolors="\e[0m"
 light_cyan="\e[96m"
+light_red="\e[91m"
 delimiter_str="="
 delimiter_len=79
 
+function __check_var() {
+    local var_name=$1
+    local default_value=$2
+    if [[ -z ${!var_name+x} ]]; then
+        __log_debug "var ${var_name} is unset, setting to default"
+        declare ${var_name}=${default_value}
+    else
+        __log_debug "var is set to '${!var_name}'"
+    fi  
+    __log_debug " ${var_name} = ${!var_name}"
+    
+}
 
 function __msg_delimiter() {
     local result
@@ -29,20 +31,21 @@ function __msg_delimiter() {
     for i in $(seq 1 $delimiter_len); do  result+="$delimiter_str"; done
     echo $result
 }
-function __dbg_error() {
+
+function __log_error() {
     [[ "${LOGLEVEL}" == "ERROR" ]] && echo -e "[$(date +'%Y-%m-%dT%H:%M:%S%z') ${LOGLEVEL}]: $*" >&2
 }
 
-function __dbg_debug() {
+function __log_debug() {
     [[ "${LOGLEVEL}" == "DEBUG" ]] && echo -e "[$(date +'%Y-%m-%dT%H:%M:%S%z') ${LOGLEVEL}]: $*"
-} 
+}
 
-function __dbg_info() {
+function __log_info() {
     [[ "${LOGLEVEL}" == "INFO" ]] && echo -e "[$(date +'%Y-%m-%dT%H:%M:%S%z') ${LOGLEVEL}]: $*"
 }
 
 function __msg_error() {
-    printf "${red}!!! %s${endcolors}\\n" "${*}" 1>&2
+    printf "${light_red}!!! %s${endcolors}\\n" "${*}" 1>&2
 }
 function __msg_info() {
     printf "${light_cyan}___ %s${endcolors}\\n" "${*}" 1>&2
@@ -52,21 +55,22 @@ function __cmd_exists()
 {
     command -v "$1" >/dev/null 2>&1
     if [[ $? -eq 0 ]]; then
-        __dbg_info "$1 found"
+        __log_info "$1 found"
         return 0
     else
-        __dbg_error "$1 not found"
+        __log_error "$1 not found"
         return 1
     fi
 }
 function __file_exist()
 {
     if [[ -z "$1" ]]; then
-        __dbg_error "filename is empty"
+        __log_error "filename is empty"
         return 1
     fi
     if ! [[ -e $1 ]]; then
-        __dbg_error "file $1 not found"
+        __log_error "file $1 not found"
         return 1
     fi
 }
+
--- test-function.sh
+++ test-function.sh
@@ -1,16 +1,19 @@
-. ./base-function.sh
+. ./base-functions.sh
 
 __msg_info "test info"
 __msg_error "test error"
 __msg_delimiter
 echo ${LOGLEVEL}
 __msg_delimiter
-LOGLEVEL="INFO" __dbg_info "dbg info"
-LOGLEVEL="DEBUG" __dbg_debug "dbg "
-LOGLEVEL="ERROR" __dbg_error "dbg error"
+LOGLEVEL="INFO" __log_info "log info"
+LOGLEVEL="DEBUG" __log_debug "log debug"
+LOGLEVEL="ERROR" __log_error "log error"
 __msg_delimiter
 LOGLEVEL="ERROR" __file_exist ./test-function.sh
 LOGLEVEL="ERROR" __file_exist ./nullsh
 __msg_delimiter
 LOGLEVEL="INFO" __cmd_exists "ls"
 LOGLEVEL="ERROR" __cmd_exists "null"
+__msg_delimiter
+#LEVEL1="2"
+LOGLEVEL="DEBUG" __check_var "LEVEL1" "5"
